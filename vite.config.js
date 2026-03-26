4bba9c73de655f5b6fc1d1dab62c044592764ee7
XZ1T3 <132022947+XZ1T3@users.noreply.github.com>
2026-03-25 19:09:04 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -39,3 +39,15 @@ export default defineConfig({
     },
   },
 });
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './', 
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
+});
